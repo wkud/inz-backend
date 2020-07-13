dacced7c1b179eab954cb466708e5544593a3452
Wojtek <vopox07@gmail.com>
2020-07-13 17:21:22 +0200

    Add description to test functions

--- app.py
+++ app.py
@@ -9,23 +9,29 @@ array = {}
 
 
 class Endpoint(Resource):
+    # http://127.0.0.1:5000/
     def get(self):
         return {"about": "Hello World!"}
+        # response: {"about": "Hello World!"}
 
+    # http://127.0.0.1:5000?index=4&value=10
     def post(self):
         args = request.args
         index = int(args['index'])
         value = int(args['value'])
         array[index] = value
         return array
+        # response: { "4": 10 }
 
 
 class UrlEndpoint(Resource):
+    # http://127.0.0.1:5000/some_name
     def post(self, name):
         if request.method == 'POST':
             data = request.get_json()
         collection.append({name: data})
         return collection
+        # response: { "some_name": 10 }
 
 
 api.add_resource(Endpoint, "/")
